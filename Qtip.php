49e394e33c0d637f0d9ad8d504a4f0a5f15216fb
Sammaye <sam.millman@googlemail.com>
2014-03-24 10:31:39 +0000

    Initial Commit

---
+++ Qtip.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace sammaye\qtip;
+
+use Yii;
+use \Exception;
+use yii\base\Widget;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use common\components\qtip\QtipAsset;
+
+class Qtip extends Widget
+{
+	public $hook;
+	public $options = [];
+	
+	public function __get($k)
+	{
+		try{
+			return parent::__get($k);
+		}catch(Exception $e){
+			return isset($this->options[$k]) ? $this->options[$k] : null;
+		}
+	}
+	
+	public function __set($k, $v)
+	{
+		try{
+			parent::__set($k, $v);
+		}catch(Exception $e){
+			$this->options[$k] = $v;
+		}
+	}
+	
+	public function run()
+	{
+		QtipAsset::register($this->getView());
+		$this->getView()->registerJs("$('$this->hook').qtip(" . Json::encode($this->options) . ");");
+	}
+}
---
+++ QtipAsset.php
@@ -0,0 +1,12 @@
+<?php
+namespace sammaye\qtip;
+
+use Yii;
+use yii\web\AssetBundle;
+
+class QtipAsset extends AssetBundle
+{
+	public $sourcePath = __DIR__;
+	public $css = ['jquery.qtip.css'];
+	public $js = ['jquery.qtip.js'];
+}
