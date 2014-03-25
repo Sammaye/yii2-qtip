20f9bb5856885b244eecf56c4cb2ef492dd0e970
Sammaye <sam.millman@googlemail.com>
2014-03-25 11:57:32 +0000

    Fixed Jquery depends

--- QtipAsset.php
+++ QtipAsset.php
@@ -9,4 +9,7 @@ class QtipAsset extends AssetBundle
 	public $sourcePath = __DIR__;
 	public $css = ['jquery.qtip.css'];
 	public $js = ['jquery.qtip.js'];
+	public $depends = [
+		'yii\web\JqueryAsset'
+	];	
 }
